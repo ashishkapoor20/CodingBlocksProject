c07a94800b342d65b04b5ea07f24c102e6b556cc
ashish kapoor <ashishkap1999@gmail.com>
2020-07-13 14:35:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingblocks'
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    message = db.Column(db.String(20), nullable=False)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contact(name=name, email=email, phone=phone, message=message)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
+
+
+@app.route("/index")
+def home():
+    return render_template('index.html')
+
+
+app.run(debug=True)
